579c88671e8169e39df3720ca177efe056f23e2e
Parth Bisht <bishtparth795@gmail.com>
2022-06-09 23:31:55 +0530

    first commit

---
+++ mongoose/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const connection = mongoose.connect("mongodb://localhost:27017/IMDB");
+
+const MovieSchema = new mongoose.Schema({
+    title: String,
+    rating: Number,
+    releaseData: Date,
+    earning: Number,
+    cast: [String],
+    language: String
+})
+
+const Movie = mongoose.model("movie",MovieSchema);
+module.exports = {Movie, connection};
